e32e665e2800281b53e296d0f59c9f79556be466
Ashkan-Rahnavardi <ashkan.rahnavardi@gmail.com>
2022-11-28 13:26:12 -0800

    Lab 11

---
+++ client/src/Search.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+
+function Search({ types, checkedState, setCheckedState }) {
+
+  const onChangeHandle = (type) => {
+    const index = types.current.indexOf(type);
+    // index ? !item : item is a ternary operator, if i = index then flip the value of item, otherwise keep it the same
+    const newCheckedState = checkedState.map((item, i) => i === index ? !item : item);
+    setCheckedState(newCheckedState);
+  }
+
+  return (
+    <div>
+      {
+        types.current.map(type => {
+          return (
+            <span key={type}>
+              <input type="checkbox" name="pokeTypes" value={type} id={type} onChange={() => { onChangeHandle(type) }} />
+              <label htmlFor={type}>{type}</label>
+              <br />
+            </span>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default Search
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'
+import FilteredPagination from './FilteredPagination'
+import Search from './Search'
+import axios from 'axios'
+
+
+function Main() {
+  // Array representing if pokemon type is checked or not
+  const [checkedState, setCheckedState] = useState([]);
+  // Array of pokemon types
+  const types = useRef([])
+
+  // Fetch pokemon types, set checkedState to false for each type, and set types
+  async function getPokemonTypes() {
+    const result = await axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json');
+    types.current = result.data.map(type => type.english);
+    setCheckedState(new Array(result.data.length).fill(false))
+  }
+
+  // passing an empty array as a second argument to useEffect will make it run only once
+  useEffect(() => {
+    getPokemonTypes();
+  }, [])
+
+  return (
+    <>
+      <Search types={types} checkedState={checkedState} setCheckedState={setCheckedState} />
+      <FilteredPagination types={types} checkedState={checkedState} />
+    </>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <>
-    <App />
-  </>
+  <Main />
 );
