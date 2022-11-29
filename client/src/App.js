890c72c72e0cd2cb1dd15e7eefd53436054a1c38
Ashkan-Rahnavardi <ashkan.rahnavardi@gmail.com>
2022-11-29 08:30:32 -0800

    started adding a node backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,35 +3,52 @@ import Page from './Page'
 import Pagination from './Pagination';
 import axios from 'axios'
 function App() {
-  const [pokemons, setPokemons] = useState([])
+  const [data, setData] = React.useState(null);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pokemonsPerPage] = useState(10);
-
-  useEffect(() => {
-    axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
-      .then(res => res.data)
-      .then(res => {
-        setPokemons(res)
-      })
-      .catch(err => console.log("err", err))
-  }, [])
-
-  const indexOfLastRecord = currentPage * pokemonsPerPage;
-  const indexOfFirstRecord = indexOfLastRecord - pokemonsPerPage;
-  const currentPokemons = pokemons.slice(indexOfFirstRecord, indexOfLastRecord)
-  const numberOfPages = Math.ceil(pokemons.length / pokemonsPerPage);
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
 
   return (
-    <>
-      < Page currentPokemons={currentPokemons} currentPage={currentPage} />
-      < Pagination
-        numberOfPages={numberOfPages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
 }
+
+//   const [pokemons, setPokemons] = useState([])
+
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [pokemonsPerPage] = useState(10);
+
+//   useEffect(() => {
+//     axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
+//       .then(res => res.data)
+//       .then(res => {
+//         setPokemons(res)
+//       })
+//       .catch(err => console.log("err", err))
+//   }, [])
+
+//   const indexOfLastRecord = currentPage * pokemonsPerPage;
+//   const indexOfFirstRecord = indexOfLastRecord - pokemonsPerPage;
+//   const currentPokemons = pokemons.slice(indexOfFirstRecord, indexOfLastRecord)
+//   const numberOfPages = Math.ceil(pokemons.length / pokemonsPerPage);
+
+//   return (
+//     <>
+//       < Page currentPokemons={currentPokemons} currentPage={currentPage} />
+//       < Pagination
+//         numberOfPages={numberOfPages}
+//         currentPage={currentPage}
+//         setCurrentPage={setCurrentPage}
+//       />
+//     </>
+//   )
+// }
 
 export default App
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+
